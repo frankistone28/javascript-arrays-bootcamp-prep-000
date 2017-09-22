d0ddc1ec7495c1a9fb46fd52c4ec72bf9176c064
Franklyn Thompson <frankistone28@outlook.com>
2017-09-22 00:12:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = [ "snickers","hundred grand","kit kat","skittles"];
 
 function addElementToBeginningOfArray(array,element){
-    array.unshift(element);
-    return array;
+    return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
